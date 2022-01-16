79d72e451241820af252cf000104117e235e2bb6
Dmitriy <dexamd@MacBook-Pro.local>
2022-01-17 00:11:11 +0300

    add react-native-maps module

--- src/screens/map/Map.screen.tsx
+++ src/screens/map/Map.screen.tsx
@@ -1,14 +1,30 @@
+import { useStyleSheet } from '@ui-kitten/components'
 import React from 'react'
 import { View } from 'react-native'
-import { Text } from '@ui-kitten/components'
+import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from 'react-native-maps'
+import themedStyles from './Map.style'
 
 const MapScreen = () => {
+	const styles = useStyleSheet(themedStyles)
+
 	return (
 		<>
-			<View>
-				<Text>
-					MapScreen
-				</Text>
+			<View style={styles.mapContainer}>
+
+				<MapView
+					style={styles.map}
+					provider={PROVIDER_DEFAULT}
+					loadingEnabled={true}
+					showsCompass={true}
+					showsUserLocation={true}
+					showsMyLocationButton={true}
+					initialRegion={{
+						latitude: 37.78825,
+						longitude: -122.4324,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421
+					}}
+				/>
 			</View>
 		</>
 	)
---
+++ src/screens/map/Map.style.ts
@@ -0,0 +1,91 @@
+import { StyleService } from '@ui-kitten/components'
+import theme from '../../theme/themeProps'
+
+const { DEFAULT_BOX_SIZE } = theme
+
+const themedStyles = StyleService.create({
+	pointList: {
+		height: 500,
+		backgroundColor: 'color-basic-100'
+	},
+	mapContainer: {
+		height: '100%',
+	},
+	map: {
+		height: '95%'
+	},
+	createRouteButton: {
+		width: '50%',
+		position: 'absolute',
+		bottom: 50,
+		left: '25%',
+	},
+	bottomSheet: {
+		width: '100%',
+		display: 'flex',
+		alignItems: 'center'
+	},
+	statusBar: {
+		width: DEFAULT_BOX_SIZE,
+		height: 60,
+		// backgroundColor: '#000',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+	},
+	timer: {
+		width: '30%',
+		// backgroundColor: 'red',
+		alignItems: 'flex-start',
+		justifyContent: 'center'
+	},
+	distance: {
+		width: '30%',
+		// backgroundColor: 'blue',
+		alignItems: 'flex-start',
+		justifyContent: 'center'
+	},
+	pointQuant: {
+		width: '30%',
+		// backgroundColor: 'green',
+		alignItems: 'flex-start',
+		justifyContent: 'center'
+	},
+	statusBarItemTitle: {
+		fontSize: 10,
+		fontWeight: 'bold',
+	},
+	statusBarItemContent: {
+		fontSize: 15,
+		fontWeight: 'bold',
+	},
+	pointListBox: {
+		width: DEFAULT_BOX_SIZE,
+		height: '75%',
+		// backgroundColor: 'red',
+	},
+	pointBox: {
+		width: DEFAULT_BOX_SIZE,
+		display: 'flex',
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	pointControl: {
+		display: 'flex',
+		flexDirection: 'row',
+	},
+	controlIcon: {
+
+	},
+	addPointButton: {
+
+	},
+	sheetButtonGroup: {
+		width: DEFAULT_BOX_SIZE,
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-around',
+	},
+})
+
+export default themedStyles
