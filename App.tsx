4ae907fcbc29b99c983cfd85f834cded4bfaac2d
deviro <ilia.tro.dev@gmail.com>
2022-01-20 01:11:38 +0300

    beatify

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect} from 'react'
+import React, { FC, useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
@@ -9,27 +9,28 @@ import { getFromStorage } from './src/core/utils/Storage.service'
 import { useAppDispatch } from './src/core/hooks/redux'
 // import { setIsAuth } from './src/store/auth/AuthSlice'
 import Navigation from './src/navigation/Navigation'
+// eslint-disable-next-line import/no-named-default
 import { default as theme } from './src/theme/configuredTheme.json'
 import ApplicationSetup from './src/components/ApplicationSetup/ApplicationSetup'
-
+import { KeyboardAvoidingView } from 'react-native'
 
 const App: FC = () => {
 	const store = setupStore()
 
-  return (
+	return (
 		<>
 			<Provider store={store}>
 				<IconRegistry icons={EvaIconsPack} />
 				<ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }}>
-						<SafeAreaProvider>
-							<Navigation />
-						</SafeAreaProvider>
+					<SafeAreaProvider>
+						<Navigation />
+					</SafeAreaProvider>
 				</ApplicationProvider>
 
 				<ApplicationSetup />
 			</Provider>
 		</>
-  )
+	)
 }
 
 export default App
