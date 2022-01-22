f5ed272c9bae1d7dc2c3aa998be57bb26e3cf541
Dmitriy <dexamd@MacBook-Pro.local>
2022-01-22 21:38:04 +0300

    add BottomSheet

---
+++ src/components/PointsPassingList/PointPassingList.style.ts
@@ -0,0 +1,8 @@
+import { StyleService } from '@ui-kitten/components'
+
+const themedStyles = StyleService.create({
+  playIcon: {
+  }
+})
+
+export default themedStyles
---
+++ src/components/PointsPassingList/PointPassingList.tsx
@@ -0,0 +1,74 @@
+import React, { FC, useState } from 'react'
+import { Text, View } from 'react-native'
+import { PointPass } from '../../screens/routePassing/RoutePassing.helper'
+import Point from '../Point/Point'
+import { Icon, useStyleSheet } from '@ui-kitten/components'
+import { getImageSrc, getAudioSrc } from '../../core/utils/Main.helper'
+import themedStyles from './PointPassingList.style'
+import Sound from 'react-native-sound'
+
+interface PointsPassingListProps {
+  points: PointPass[]
+  soundList: Sound[]
+}
+
+const PointsPassingList: FC<PointsPassingListProps> = ({ points, soundList }) => {
+  const styles = useStyleSheet(themedStyles)
+
+  const [isAudioPlaying, setAudioPlaying] = useState(false)
+  const [actualAudioIndex, setActualAudioIndex] = useState(-1)
+
+  // console.log('points', points[0].data.point.media)
+
+  const _playAudio = (index: number) => {
+    if (isAudioPlaying && actualAudioIndex !== index) {
+      soundList[actualAudioIndex].pause()
+    }
+
+    if (!soundList[index].isPlaying()) {
+      soundList[index].play(() => {
+        setAudioPlaying(false)
+      })
+      setAudioPlaying(true)
+      setActualAudioIndex(index)
+      return
+    }
+
+    soundList[index].pause()
+    setAudioPlaying(false)
+    setActualAudioIndex(-1)
+  }
+
+  return (
+    <View>
+      {
+        points &&
+        points.map(pointInfo =>
+          <Point
+            key={pointInfo.index}
+            style={{ opacity: pointInfo.isPassed ? 1 : 0.3 }}
+            title={ pointInfo.data.point.title }
+            photo={ getImageSrc(pointInfo.data.point.media[1].id, 100) }>
+            {
+              !soundList || soundList[pointInfo.index] &&
+              <Icon
+                style={styles.playIcon}
+                width={40}
+                height={40}
+                fill={'#ecf0f1'}
+                name={
+                  actualAudioIndex === pointInfo.index
+                  ? 'pause-circle-outline'
+                  : 'play-circle-outline'
+                }
+                onPress={ () => pointInfo.isPassed && _playAudio(pointInfo.index) }
+              />
+            }
+          </Point>
+        )
+      }
+    </View>
+  )
+}
+
+export default PointsPassingList
