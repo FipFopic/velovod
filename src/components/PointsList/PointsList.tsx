2746fbef7b3a398fdfebac0df6a068c08b45bc7a
alextroart <tro.alex80@gmail.com>
2022-02-15 17:38:36 +0300

    Update Pointlist image

--- src/components/PointsList/PointsList.tsx
+++ src/components/PointsList/PointsList.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { View } from 'react-native'
+import { getMediaSrc } from '../../../src/core/utils/Main.helper'
 import { IPoint } from '../../core/interfaces/IRoute'
 import Point from '../Point/Point'
 
@@ -8,17 +9,6 @@ interface PointsListProps {
 }
 
 const PointsList: FC<PointsListProps> = ({ points }) => {
-
-	// console.log(' ')
-	// console.log(' ')
-	// console.log(' ')
-	// console.log(' ')
-	// console.log('_____pointInfo', points[0].point.media)
-	// console.log(' ')
-	// console.log(' ')
-	// console.log(' ')
-	// console.log(' ')
-
 	return (
 		<View>
 			{
@@ -27,7 +17,7 @@ const PointsList: FC<PointsListProps> = ({ points }) => {
 					<Point
 						key={pointInfo.point.id}
 						title={pointInfo.point.title}
-						photo={'https://i.pinimg.com/564x/ff/2e/3b/ff2e3b2eb929a60c83868dbb7f7c33a0.jpg'}
+						photo={getMediaSrc(pointInfo?.point?.media, 'image', 100)}
 						style={{ opacity: 1 }}
 					/>
 				)
