6fb954061d6b80ba051a097dac8e6fc73c4f6a0b
ilia tro <ilia.tro.dev@gmail.com>
2022-06-17 17:49:19 +0200

    feat: add splash screen for android

--- App.tsx
+++ App.tsx
@@ -6,7 +6,6 @@ import { EvaIconsPack } from '@ui-kitten/eva-icons'
 import * as eva from '@eva-design/eva'
 import { setupStore } from './src/core/redux/store'
 import { getFromStorage } from './src/core/utils/Storage.service'
-import { useAppDispatch } from './src/core/hooks/redux'
 // import { setIsAuth } from './src/store/auth/AuthSlice'
 import Navigation from './src/navigation/Navigation'
 // eslint-disable-next-line import/no-named-default
@@ -14,9 +13,15 @@ import { default as theme } from './src/theme/configuredTheme.json'
 import ApplicationSetup from './src/components/ApplicationSetup/ApplicationSetup'
 import { KeyboardAvoidingView } from 'react-native'
 import 'react-native-gesture-handler'
+import SplashScreen from 'react-native-splash-screen'
 
 const App: FC = () => {
 	const store = setupStore()
+
+	//hide splash screen after load
+	useEffect(() => {
+		SplashScreen.hide()
+	})
 
 	return (
 		<>
